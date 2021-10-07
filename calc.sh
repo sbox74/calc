0fccac7de6218e3f69a9770e4b3785631a1d32dd
sbox74 <servicebox74@mail.ru>
2021-10-08 00:11:13 +0300

    calc

---
+++ calc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PARAMS=("$@")
+RES=-32768
+
+function test_expr() {
+  (( "$1" )) 2>/dev/null
+}
+
+for file in ${PARAMS[@]}; do
+  echo
+  echo "processing file: $file"
+  [ ! -f $file ] && echo "$file: file not found" && continue
+  while read expression ; do
+    echo "expression: $expression"
+    test_expr "expression"
+    [ $? -gt 0 ] && echo "incorrect expression" && continue
+    EXP_RES=$(( expression ))
+    echo "result: $EXP_RES"
+    [ $EXP_RES -gt $RES ] && RES=$EXP_RES
+  done < $file
+done
+
+echo
+echo "files processed: ${#PARAMS[@]}"
+echo "the highest value: $RES"
+
